1cdb9ee408ff340f80c203647998e7e86acff3bc
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-28 21:05:31 -0700

    add insert method

--- src/index.js
+++ src/index.js
@@ -79,6 +79,21 @@ class SinglyLinkedList{
         }
         return false;
     }
+    insert(index, val){
+        if (index < 0 || index > this.length) return false;
+        if (index === this.length) {
+            this.push(val);
+            return true;
+        }
+        if (index === 0) return !!this.unshift(val);
+        var newNode = new Node(val);
+        var prev = this.get(index - 1);
+        var temp = prev.next 
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
 }
 
 var list = new SinglyLinkedList()
